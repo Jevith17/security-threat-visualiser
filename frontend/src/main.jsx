fd193459f8dd720de6bd42d3e97da6106316bcc4
Jevith Reddy <jevithgns@gmail.com>
2026-02-10 23:47:19 +0530

    chore(frontend): import index.css in main entry point

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AttackGlobe from "./AttackGlobe";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AttackGlobe />
